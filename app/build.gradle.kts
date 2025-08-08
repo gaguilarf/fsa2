7373034ea309474d20c905cef1f9b269f1726473
GUSTAVO FERNANDO AGUILAR FLORES <gaguilarf@unsa.edu.pe>
2025-08-07 22:55:17 -0500

    feat(master): version 1.6 correcciones minimas en el diseño y mejorar de la estructura de los strings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.molytech.fsa"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
+        versionCode = 9
         versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
